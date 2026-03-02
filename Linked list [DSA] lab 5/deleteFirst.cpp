252d9bed0c1286a2839eafba526bcbe0f76f1931
Nuhad Bin Aziz <nuhadbinaziz19@gmail.com>
2026-03-02 11:20:13 +0600

    Add files via upload

---
+++ Linked list [DSA] lab 5/deleteFirst.cpp
@@ -0,0 +1,77 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node*next;
+
+    Node(int k){
+    val= k;
+    next=nullptr;
+    }
+};
+class List{
+public:
+    Node *start=nullptr;
+
+    void ins(int k){
+      Node*n=new Node(k);
+      if(start==nullptr){
+        start=n;
+      }
+      else{
+        Node*p=start;
+        while(p->next!=nullptr){
+            p=p->next;
+        }
+        p->next=n;
+      }
+    }
+ void insLast(int k){
+   Node*n=new Node(k);
+ if (start==nullptr){
+    start=n;
+ }
+ else{
+    n->next=start;
+    start=n;
+ }
+ }
+ void deleteFirst(){
+ start=start->next;
+ }
+ void deleteLast(){
+ Node*j=start;
+ while(j->next->next!=nullptr){
+
+    j=j->next;
+ }
+    j->next=nullptr;
+ }
+  void  traverse(){
+  Node*p=start;
+  while(p!=nullptr){
+    cout<<p->val<<"\t";
+    p=p->next;
+
+  }
+  cout<<endl;
+  }
+};
+int main (){
+  List t;
+  t.ins(5);
+  t.ins(10);
+  t.ins(3);
+  t.ins(15);
+  t.traverse();
+  t.insLast(9);
+  t.traverse();
+  t.deleteFirst();
+  t.traverse();
+  t.deleteLast();
+  t.traverse();
+return 0;
+}
